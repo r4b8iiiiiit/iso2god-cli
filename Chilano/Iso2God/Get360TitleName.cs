00bd618c2ecc3223d0f40ba14ad3f2a7858198ea
unknown <78566975+r4b8iiiiiit@users.noreply.github.com>
2021-04-30 20:44:40 +0100

    Updated Get360TitleName.cs to fix bug where not all ISOs would work with select query

--- Chilano/Iso2God/Get360TitleName.cs
+++ Chilano/Iso2God/Get360TitleName.cs
@@ -12,14 +12,16 @@ namespace Chilano.Iso2God
             string table = @"URI=file:IDs.db";
             using var con = new SQLiteConnection(table);
             con.Open();
-            var selectStatement = "Select Title FROM Tiltle_List WHERE TitleID = " + ID;
+            var selectStatement = "Select Title FROM Tiltle_List WHERE TitleID = " + '"' + ID.ToLower() + '"';
             using var cmd = new SQLiteCommand(selectStatement, con);
             using SQLiteDataReader reader = cmd.ExecuteReader();
             string gameTitle = "";
+            List<string> gTitle = new List<string>();
             while (reader.Read())
             {
                 gameTitle = reader.GetString(0);
             }
+            //con.Close();
             return gameTitle;
         }
         public static string getTitleID(string ID)
